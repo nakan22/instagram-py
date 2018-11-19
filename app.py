85e1eefa6fb55c732b4ddeacbdd816861b44942a
nakan <nakan.tangaromsuk@gmail.com>
2018-11-19 15:44:12 +0800

    created signup page

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ from flask import render_template, request
 from database import db, app, User
 
 
-@app.route("/")
+@app.route('/')
 def home():
+    print('hello world')
     return render_template('home.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+
+
+if __name__ == '__main__':
+    app.run()
